c4dbad27a3098c31f8599cf1df67424ef62565d7
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-18 17:30:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getSize() {
+  let w = document.documentElement.clientWidth;
+
+  let h = document.documentElement.clientHeight;
+
+  document.getElementById("sizeInfo").innerHTML =
+    "<h1>Width: " + w + " and " + " heigth: " + h + " </h1>";
+}
